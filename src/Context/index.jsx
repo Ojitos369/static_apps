6d0bc8d3bc94c32a9d84b5390e6c233f2e645e82
ojitos369 <ojitos369@gmail.com>
2022-04-01 09:12:38 -0600

    cambio en la navbar

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -7,10 +7,52 @@ function Provier(props) {
         item: mostrar,
         saveItems: setMostrar,
     } = useLocalStorage('mostrar', 'home');
+    const {
+        item: classNames,
+        saveItems: setClassNames
+    } = useLocalStorage('classNames', 'nav-app');
+
+    const {
+        item: classBases,
+        saveItems: setClassBases
+    } = useLocalStorage('classBases', 'nav-app');
+
+    const {
+        item: classSuper,
+        saveItems: setClassSuper
+    } = useLocalStorage('classSuper', 'nav-app');
+
+    const changeMostrar = text => {
+        setMostrar(text);
+        if (text === 'names') {
+            setClassNames('nav-app app-active');
+            setClassBases('nav-app');
+            setClassSuper('nav-app');
+        }
+        if (text === 'bases') {
+            setClassNames('nav-app');
+            setClassBases('nav-app app-active');
+            setClassSuper('nav-app');
+        }
+        if (text === 'super') {
+            setClassNames('nav-app');
+            setClassBases('nav-app');
+            setClassSuper('nav-app app-active');
+        }
+        if (text === 'home') {
+            setClassNames('nav-app');
+            setClassBases('nav-app');
+            setClassSuper('nav-app');
+        }
+    }
+
     return (
         <Context.Provider value={{
             mostrar,
-            setMostrar,
+            classNames,
+            classBases,
+            classSuper,
+            changeMostrar
         }}>
             {props.children}
         </Context.Provider>
--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -3,11 +3,16 @@ import './Navbar.css'
 import { Context } from '../Context';
 
 function NavBar() {
-    const { setMostrar } = React.useContext(Context);
+    const { 
+        changeMostrar,
+        classNames,
+        classBases,
+        classSuper
+    } = React.useContext(Context);
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <button className="navbar-brand" onClick={() => setMostrar('home')}>Home</button>
+                <button className="navbar-brand" onClick={() => changeMostrar('home')}>Home</button>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,17 +20,24 @@ function NavBar() {
                     <div className="navbar-nav me-auto mb-2 mb-lg-0">
                     </div>
                     <ul className="d-flex navbar-nav">
-                        <li className="nav-item app-item d-flex justify-content-center">
-                            <button className="nav-link" aria-current="page" onClick={() => setMostrar('names')}>Names Generator</button>
-                        </li>
-                        <li className="nav-item app-item d-flex justify-content-center">
-                            <button className="nav-link" onClick={() => setMostrar('bases')}>Bases Converted</button>
-                        </li>
-                        <li className="nav-item app-item d-flex justify-content-center">
-                            <button className="nav-link" onClick={() => setMostrar('super')}>Super List</button>
-                        </li>
+                        <div class="dropdown nav-item app-item d-flex justify-content-center">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="navbar-apps" data-bs-toggle="dropdown" aria-expanded="false">
+                                Apps
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="navbar-apps">
+                                <li className="nav-item app-item d-flex justify-content-center">
+                                    <button className={classNames} aria-current="page" onClick={() => changeMostrar('names')}>Names Generator</button>
+                                </li>
+                                <li className="nav-item app-item d-flex justify-content-center">
+                                    <button className={classBases} onClick={() => changeMostrar('bases')}>Bases Converted</button>
+                                </li>
+                                <li className="nav-item app-item d-flex justify-content-center">
+                                    <button className={classSuper} onClick={() => changeMostrar('super')}>Super List</button>
+                                </li>
+                            </ul>
+                        </div>
                         <li className="nav-item">
-                            <a className="nav-link" href='https://github.com/ojitos369/static_apps/' target='_blank' rel='noopener noreferrer'>Repo en Github</a>
+                            <a className="nav-link nav-repo" href='https://github.com/ojitos369/static_apps/' target='_blank' rel='noopener noreferrer'>Repo en Github</a>
                         </li>
                     </ul>
                 </div>
