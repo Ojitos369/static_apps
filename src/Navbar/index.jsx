ed87ac655e831e7cd59bdbb33e627421f34f2c3a
ojitos369 <ojitos369@gmail.com>
2022-04-15 20:13:49 -0500

    cambio

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -25,10 +25,10 @@ function NavBar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div className="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" style={customStyle}>
+                    <div className="navbar-nav me-auto mb-2 mb-lg-0" style={customStyle}>
                     </div>
-                    <ul className="d-flex navbar-nav">
+                    <ul className="d-flex navbar-nav" style={customStyle}>
                         <li className="nav-item">
                         <button className='nav-link' aria-current="page" onClick={() => changeMostrar(10)} style={customStyle}>
                             {textos.settings}
