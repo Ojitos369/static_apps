504210731efdd66b5d0942ecfa6528d714bb7226
ojitos369 <ojitos369@gmail.com>
2023-11-13 17:45:37 -0600

    add more

--- apis/gpt_con/api.py
+++ apis/gpt_con/api.py
@@ -19,8 +19,14 @@ class Ejecutar(PostApi):
             "message": "upps no hay datos..."
         }
 
+
 class Consulta(PostApi):
     def main(self):
+        query = get_d(self.data, "query", default="no se encontro query")
+        parametros = get_d(self.data, "parametros", default="no se encontro parametros")
+        
+        pln(f"query: {query}")
+        pln(f"parametros: {parametros}")
         pln("Consulta")
         self.response = {
             "message": "upps no hay datos..."
