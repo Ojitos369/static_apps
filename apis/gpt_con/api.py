60a6e8714fa1c70b096bec8edb5db3f1f9cda826
ojitos369 <ojitos369@gmail.com>
2023-11-13 17:47:54 -0600

    POST PUT

--- apis/gpt_con/api.py
+++ apis/gpt_con/api.py
@@ -22,8 +22,13 @@ class Ejecutar(PostApi, GetApi):
 
 class Consulta(PostApi, GetApi):
     def main(self):
-        query = get_d(self.data, "query", default="no se encontro query")
-        parametros = get_d(self.data, "parametros", default="no se encontro parametros")
+        data = {}
+        try:
+            data = self.data
+        except:
+            pass
+        query = get_d(data, "query", default="no se encontro query")
+        parametros = get_d(data, "parametros", default="no se encontro parametros")
         
         pln(f"query: {query}")
         pln(f"parametros: {parametros}")
