4e5c61ca873b8a7742a662e46ad2a169adde960f
ojitos369 <ojitos369@gmail.com>
2023-12-20 11:39:21 -0600

    setproctitle

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from ojitos369.errors import CatchErrors as CE
+
+import setproctitle
+setproctitle.setproctitle('static_apps')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 prod_mode = True if str(os.environ.get('RUN_PROD_MODE', True)).title() == 'True' else False
