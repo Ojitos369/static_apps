5332e6b49c6a4b05f2066a22f599992c6c6da65b
ojitos369 <ojitos369@gmail.com>
2024-09-13 09:41:46 -0600

    [CHANGE] event test code

--- apis/test/api.py
+++ apis/test/api.py
@@ -9,6 +9,7 @@ import hashlib
 # User
 from app.core.bases.apis import PostApi, GetApi, get_d, pln, prod_mode
 
+
 class JS:
     code = """
         const PIXEL_ID = '123';const ACCESS_TOKEN = '1a2b3c';
@@ -132,19 +133,19 @@ class JS:
 class Test(GetApi):
     def main(self):
         self.show_me()
-        
-        PIXEL_ID = os.environ.get('FACEBOOK_PIXEL_ID')
-        CLIENT_KEY = os.environ.get('FACEBOOK_CLIENT_KEY')
-        TOKEN_KEY = os.environ.get('FACEBOOK_TOKEN_KEY')
+
+        PIXEL_ID = os.environ.get("FACEBOOK_PIXEL_ID")
+        CLIENT_KEY = os.environ.get("FACEBOOK_CLIENT_KEY")
+        TOKEN_KEY = os.environ.get("FACEBOOK_TOKEN_KEY")
         time = int(datetime.datetime.now().timestamp())
         purchase_event = {
             "event_name": "Purchase",
             "event_time": time,
             "user_data": {
-                'client_ip_address': None,
-                'client_user_agent': None,
-                'em': hashlib.sha256('ejemplo@example.com'.encode()).hexdigest(),
-                'ph': hashlib.sha256('123456789'.encode()).hexdigest()
+                "client_ip_address": None,
+                "client_user_agent": None,
+                "em": hashlib.sha256("ejemplo@example.com".encode()).hexdigest(),
+                "ph": hashlib.sha256("123456789".encode()).hexdigest(),
             },
             "custom_data": {
                 "currency": "USD",
@@ -158,15 +159,15 @@ class Test(GetApi):
             "data_processing_options_country": 0,
             "data_processing_options_state": 0,
         }
-        
+
         add_to_cart_event = {
             "event_name": "AddToCart",
             "event_time": datetime.datetime.now().timestamp(),
             "user_data": {
-                'client_ip_address': None,
-                'client_user_agent': None,
-                'em': hashlib.sha256('ejemplo@example.com'.encode()).hexdigest(),
-                'ph': hashlib.sha256('123456789'.encode()).hexdigest()
+                "client_ip_address": None,
+                "client_user_agent": None,
+                "em": hashlib.sha256("ejemplo@example.com".encode()).hexdigest(),
+                "ph": hashlib.sha256("123456789".encode()).hexdigest(),
             },
             "custom_data": {
                 "currency": "USD",
@@ -180,15 +181,15 @@ class Test(GetApi):
             "data_processing_options_country": 0,
             "data_processing_options_state": 0,
         }
-        
+
         page_view_event = {
             "event_name": "PageView",
             "event_time": datetime.datetime.now().timestamp(),
             "user_data": {
-                'client_ip_address': None,
-                'client_user_agent': None,
-                'em': hashlib.sha256('ejemplo@example.com'.encode()).hexdigest(),
-                'ph': hashlib.sha256('123456789'.encode()).hexdigest()
+                "client_ip_address": None,
+                "client_user_agent": None,
+                "em": hashlib.sha256("ejemplo@example.com".encode()).hexdigest(),
+                "ph": hashlib.sha256("123456789".encode()).hexdigest(),
             },
             "event_source_url": self.get_event_source_url(),
             "opt_out": False,
@@ -198,47 +199,44 @@ class Test(GetApi):
             "data_processing_options_country": 0,
             "data_processing_options_state": 0,
         }
-        
+
         events = [
             purchase_event,
             # add_to_cart_event,
             # page_view_event,
         ]
-        
+
         data = {
-            'data': events,
-            'test_event_code': 'TEST11495',
+            "data": events,
+            "test_event_code": "TEST12155",
         }
-        
-        url = f'https://graph.facebook.com/v20.0/{PIXEL_ID}/events?access_token={TOKEN_KEY}'
+
+        url = f"https://graph.facebook.com/v20.0/{PIXEL_ID}/events?access_token={TOKEN_KEY}"
         print(url)
-        
-        self.response = {
-            'data': data,
-            'url': url
-        }
-        
+
+        self.response = {"data": data, "url": url}
+
         payload = json.dumps(data)
         headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {TOKEN_KEY}',
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {TOKEN_KEY}",
         }
-        
+        # 6385
+
         response = requests.post(url, data=payload, headers=headers)
         r = response.json()
         if response.status_code == 200:
-            pln('Success:', r)
+            pln("Success:", r)
         else:
-            pln('Error:', r)
+            pln("Error:", r)
 
         self.response = r
 
-
     def generate_unique_event_id(self):
-      return str(uuid.uuid4())
+        return str(uuid.uuid4())
 
     def get_event_source_url(self):
-      return 'https://sa.ojitos369.com'
+        return "https://sa.ojitos369.com"
 
 
 """ 
