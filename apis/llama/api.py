ff5e37a22d2967fae30f62bf9eeac80cbffdfdf0
ojitos369 <ojitos369@gmail.com>
2024-09-24 14:46:03 -0600

    llama to personal and hist

--- apis/llama/api.py
+++ apis/llama/api.py
@@ -1,25 +1,78 @@
 # Python
-from langchain_community.chat_models.ollama import ChatOllama
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+import os
+import uuid
+import json
+import requests
 
 # User
 from app.core.bases.apis import PostApi, GetApi, get_d, pln, prod_mode
+from app.settings import MEDIA_DIR
 
 
 class Llama(GetApi):
     def main(self):
-        chat = ChatOllama(model="llama3.1")
         msg = self.data["msg"]
-        pln("user:", msg)
-        messages = [
-            SystemMessage(content="Eres un agente serio. Eres directo y tus respuestas son concisas y breves."),
-            HumanMessage(content=msg)
-        ]
-        response = chat.invoke(messages)
-        respuesta = response.content
-        
-        pln("llama:", respuesta)
+        cid = get_d(self.data, "cid", default=None)
+        self.path = f"{MEDIA_DIR}/json"
+        # http://localhost:8369/api/llama/llama/?cid=550341&msg=hola%20que%20tal
 
-        self.response = {
-            "msg": respuesta
+        if not cid:
+            cid = uuid.uuid4().hex[:6]
+            self.file_name = f"cid_{cid}.json"
+            self.c_data = {"hist": []}
+            if not os.path.exists(self.path):
+                os.makedirs(self.path)
+
+            self.save_chat_data()
+        else:
+            self.file_name = f"cid_{cid}.json"
+        
+        self.load_chat_data()
+        
+        self.c_data["hist"].append({"role": "user", "content": msg})
+        self.save_chat_data()
+
+        respuesta = None
+
+        link = "https://hlhxxj89-11434.usw3.devtunnels.ms/api/chat"
+        
+        data = {
+            "model": "llama3.1",
+            "messages": self.c_data["hist"],
+            "stream": False,
+            "options": {
+                # "seed": 123,
+                "temperature": 0
+            }
         }
+
+        timeout = 300
+        r = requests.post(link, json=data, timeout=timeout)
+        text = r.text
+        print(text)
+        respuesta = json.loads(text)
+        pln(respuesta)
+        
+        message = respuesta["message"]
+        self.c_data["hist"].append(message)
+        self.save_chat_data()
+        
+        self.response = {
+            "cid": cid,
+            "nota": "Pasar el cid en los parametros para conservar el historial",
+            "msg": message["content"],
+        }
+
+    def save_chat_data(self):
+        with open(f"{self.path}/{self.file_name}", "w") as f:
+            json.dump(self.c_data, f, indent=4)
+    
+    def load_chat_data(self):
+        with open(f"{self.path}/{self.file_name}", "r") as f:
+            self.c_data = json.load(f)
+
+
+""" 
+Aprendizaje automático con Python de Aurélien Géron
+Machine Learning de Tom Mitchell
+"""
